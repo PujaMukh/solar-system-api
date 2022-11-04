282d579b57b3c66d44259ba113139678cf8c238b
Puja Mukherjee <pujam836@gmail.com>
2022-11-04 14:09:55 -0700

    refactored code

--- app/models/planet.py
+++ app/models/planet.py
@@ -8,3 +8,25 @@ class Planet(db.Model):
     gravity = db.Column(db.Float)
     color = db.Column(db.String)
     has_moon = db.Column(db.Boolean)
+
+    def to_dict(self):
+        planet_as_dict = {}
+        planet_as_dict["id"] = self.id
+        planet_as_dict["name"] = self.name
+        planet_as_dict["diameter"] = self.diameter
+        planet_as_dict["gravity"] = self.gravity
+        planet_as_dict["color"] = self.color
+        planet_as_dict["has_moon"] = self.has_moon
+
+        return planet_as_dict
+
+    @classmethod
+    def from_dict(cls,data_dict):
+        # check if data_dict has all valid attributes
+        if "name" in data_dict and "diameter" in data_dict and "gravity" in data_dict and "color" in data_dict and "has_moon" in data_dict:
+            new_obj= cls(name=data_dict["name"],diameter=data_dict["diameter"],
+            gravity=data_dict["gravity"],
+            color=data_dict["color"],
+            has_moon=data_dict["has_moon"],)
+
+            return new_obj
--- app/planet.py
+++ app/planet.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request,abort, make_response
+from app import db
+from app.models.planet import Planet
 
-class Planet:
-    def __init__(self, id, name, diameter, gravity, color, has_moon):
-        self.id = id
-        self.name = name
-        self.diameter = diameter
-        self.gravity = gravity 
-        self.color = color
-        self.has_moon = has_moon
-
+# class Planet:
+#     def __init__(self, id, name, diameter, gravity, color, has_moon):
+#         self.id = id
+#         self.name = name
+#         self.diameter = diameter
+#         self.gravity = gravity 
+#         self.color = color
+#         self.has_moon = has_moon
 # planets = [
 #     Planet(1, "Mercury", 3031.9, 3.7, "slate gray", False),
 #     Planet(2, "Venus", 7520.8, 8.87, "yellow-white",False),
@@ -20,63 +21,103 @@ class Planet:
 #     Planet(8, "Neptune", 30599,11.15, "blue", True)
 # ]
 planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
-
-@planets_bp.route("", methods = ["GET"])
-def get_all_planets():
-    response = []
-    for planet in planets:
-        planet_dict = {
-            "id": planet.id,
-            "name": planet.name,
-            "diameter": planet.diameter,
-            "gravity": planet.gravity,
-            "color": planet.color,
-            "has_moon": planet.has_moon,
-
-        }
-        response. append(planet_dict)
-    return jsonify(response), 200
-
-@planets_bp.route("/<planet_id>", methods = ["GET"])
-def get_one_planet(planet_id):
+def get_one_planet_or_abort(planet_id):
     try:
         planet_id = int(planet_id)
     except ValueError:
-        response_str = "Invalid planet id"
-        return jsonify({"message": response_str}), 400
+        response_str = f"Invalid bike_id: `{planet_id}`. ID must be an integer"
+        abort(make_response(jsonify({"message":response_str}), 400))
+    
+    matching_planet = Planet.query.get(planet_id)
 
-@bike_bp.route("", methods = ["POST"])
+    if not matching_planet:
+        response_str = f"Planet with id `{planet_id}` was not found in the database."
+        abort(make_response(jsonify({"message":response_str}), 404))
+    
+    return matching_planet
+
+@planets_bp.route("", methods = ["GET"])
+def get_all_planets():
+    name_param = request.args.get("name")
+
+    if name_param is None:
+        planets = Planet.query.all()
+    else:
+        planets = Planet.query.filter_by(name=name_param)
+    
+    response = []
+    # for planet in planets:
+    #     planet_dict = {
+    #         "id": planet.id,
+    #         "name": planet.name,
+    #         "diameter": planet.diameter,
+    #         "gravity": planet.gravity,
+    #         "color": planet.color,
+    #         "has_moon": planet.has_moon,
+
+    #     }
+    #     response. append(planet_dict)
+    for planet in planets:
+        response.append(planet.to_dict())
+    return jsonify(response), 200
+
+# @books_bp.route("", methods=["GET"])
+# def read_all_books():
+    
+#     title_query = request.args.get("title")
+#     if title_query:
+#         books = Book.query.filter_by(title=title_query)
+#     else:
+#         books = Book.query.all()
+
+#     books_response = []
+#     for book in books:
+#         books_response.append(book.to_dict())
+#     return jsonify(books_response)
+
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+def get_one_planet(planet_id):
+    chosen_planet = get_one_planet_or_abort(planet_id)
+    # planet_dict = {
+    #         "id": planet.id,
+    #         "name": planet.name,
+    #         "diameter": planet.diameter,
+    #         "gravity": planet.gravity,
+    #         "color": planet.color,
+    #         "has_moon": planet.has_moon,
+    #     }
+
+    return jsonify ({f"message": chosen_planet.to_dict()}), 400
+
+
+
+@planets_bp.route("", methods = ["POST"])
 def add_planet():
     request_body = request.get_json()
+    new_planet = Planet.from_dict(request_body)
 
-    new_planet = Planet(
-        name = request_body["name"],
-        diameter = request_body["diameter"],
-        gravity = request_body["gravity"],
-        color = request_body["color"],
-        has_moon = request_body["has_moon"]
+    # Planet(
+    #     name = request_body["name"],
+    #     diameter = request_body["diameter"],
+    #     gravity = request_body["gravity"],
+    #     color = request_body["color"],
+    #     has_moon = request_body["has_moon"]
 
+    # for planet in planets:
+    #     if planet.id == planet_id:
+    #         planet_dict = {
+    #             "id": planet.id,
+    #             "name": planet.name,
+    #             "diameter": planet.diameter,
+    #             "gravity": planet.gravity,
+    #             "color": planet.color,
+    #             "has_moon": planet.has_moon,
+    #         }
+    #         return jsonify(planet_dict), 200
 
-    )
+    db.session.add(new_planet)
+    db.session.commit()
 
+    return {"id": new_planet.id}, 201
 
-    for planet in planets:
-        if planet.id == planet_id:
-            planet_dict = {
-                "id": planet.id,
-                "name": planet.name,
-                "diameter": planet.diameter,
-                "gravity": planet.gravity,
-                "color": planet.color,
-                "has_moon": planet.has_moon,
-            }
-            return jsonify(planet_dict), 200
-
-    response_message = "Could not find the planet id"
-    return jsonify({"message": response_message}), 400
-
-
-
-
-
-
+    
